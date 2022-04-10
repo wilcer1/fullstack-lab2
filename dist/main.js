a8e458303b50b38d6b689aec46cb3a4677d956ae
wilcer1 <wille.cervin@live.se>
2022-04-10 18:40:25 +0200

    zzzz

--- dist/main.js
+++ dist/main.js
@@ -15,7 +15,7 @@
   \**********************/
 /***/ (() => {
 
-eval("\n\n//# sourceURL=webpack://fullstack-lab2/./src/index.js?");
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nError: Plugin/Preset files are not allowed to export objects, only functions. In C:\\\\Users\\\\wille\\\\OneDrive\\\\Dokument\\\\GitHub\\\\fullstack-lab2\\\\node_modules\\\\babel-preset-react\\\\lib\\\\index.js\\n    at createDescriptor (C:\\\\Users\\\\wille\\\\OneDrive\\\\Dokument\\\\GitHub\\\\fullstack-lab2\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\config-descriptors.js:211:11)\\n    at createDescriptor.next (<anonymous>)\\n    at step (C:\\\\Users\\\\wille\\\\OneDrive\\\\Dokument\\\\GitHub\\\\fullstack-lab2\\\\node_modules\\\\gensync\\\\index.js:261:32)\\n    at C:\\\\Users\\\\wille\\\\OneDrive\\\\Dokument\\\\GitHub\\\\fullstack-lab2\\\\node_modules\\\\gensync\\\\index.js:273:13\\n    at async.call.result.err.err (C:\\\\Users\\\\wille\\\\OneDrive\\\\Dokument\\\\GitHub\\\\fullstack-lab2\\\\node_modules\\\\gensync\\\\index.js:223:11)\");\n\n//# sourceURL=webpack://fullstack-lab2/./src/index.js?");
 
 /***/ })
 
@@ -24,7 +24,7 @@ eval("\n\n//# sourceURL=webpack://fullstack-lab2/./src/index.js?");
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
-/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
 /******/ 	var __webpack_exports__ = {};
 /******/ 	__webpack_modules__["./src/index.js"]();
 /******/ 	
