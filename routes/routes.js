8d1fb791157ade3cde9b69e51e6eb6720b75a879
wilcer1 <wille.cervin@live.se>
2022-04-11 17:59:34 +0200

    ITS ALIIIIIIVE

--- routes/routes.js
+++ routes/routes.js
@@ -59,15 +59,59 @@ router.post("/registration", async (req, res) => {
 });
 
 router.get("/students", async (req, res) => {
-    console.log("hello world");
     const student = await Student.find();
     res.send(student);
 
 });
 
-router.patch("/student", async (req, res) => {
-   
 
-    console.log(req.body.name);
+router.get("/courses", async (req, res) => {
+    const courses = await Course.find();
+    res.send(courses);
+
 });
+router.get("/registrations", async (req, res) => {
+    const registrations = await Registration.find();
+    res.send(registrations);
+
+});
+
+router.get("/studentinfo", async (req, res) => {
+    const student = await Student.find().select("unit_id full_name -_id");
+    const registration = await Registration.find().select(`course_code student_id unix_timestamp -_id`);
+    
+    for(let stud of student){
+        for(let reg of registration){
+            if(stud.unit_id == reg.student_id){
+                stud.course_code = reg.course_code;
+                stud.regtime = reg.unix_timestamp;
+            }
+        }
+    }
+    const course = await Course.find().select("course_code course_name -_id");
+    for (let stud of student){
+        for (let crs of course){
+            if(stud.course_code == crs.course_code){
+                stud.course_name = crs.course_name;
+            }
+        }
+    }
+
+
+
+    const studentinfo = [];
+    for(let i = 0; i < student.length; i++){
+        studentinfo.push
+       ({
+        student_id: student[i].unit_id,
+        student_name: student[i].full_name,
+        course_name: student[i].course_name,
+        registration_time: student[i].regtime
+    });
+    }
+    
+    res.send(studentinfo);
+    
+});
+
 module.exports = router;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,80 @@
 import React from "react"; 
 import ReactDOM from "react-dom"; 
+import { useEffect } from "react";
+
  
 function Welcome(props) { 
+    useEffect(() => {
+    setInterval(() => {
+      fetch('http://localhost:5000/api/studentinfo', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        
+      })
+      .then(res => res.json())
+      .then(response => {
+          const element = <Table list={response} />; 
+          ReactDOM.render(element, document.getElementById("index"))
+          console.log(response);
+          
+      
+          
+        });                   
+
+    }, 3000);
+
+  }, []);
+
   return <h1>Hello, {props.name}</h1>; 
 } 
  
 const element = <Welcome name="YOUR NAME" />; 
  
 ReactDOM.render(element, document.getElementById("index")); 
+
+
+
+
+function Table(props) {
+  return (
+    <div className="App">
+      <table>
+        <tr>
+          <th>Student id</th>
+          <th>Student name</th>
+          <th>Course name</th>
+          <th>Registration time</th>
+        </tr>
+        {props.list.map((val, key) => {
+          return (
+            <tr key={key}>
+              <td>{val.student_id}</td>
+              <td>{val.student_name}</td>
+              <td>{val.course_name}</td>
+              <td>{val.registration_time}</td>
+            </tr>
+          )
+        })}
+      </table>
+    </div>
+  );
+}
+
+fetch('http://localhost:5000/api/studentinfo', {
+  method: 'GET',
+  headers: {
+      'Content-Type': 'application/json'
+  },
+  
+})
+.then(res => res.json())
+.then(response => {
+    const element = <Table list={response} />; 
+    ReactDOM.render(element, document.getElementById("index"))
+    console.log(response);
+    
+
+    
+  });                   
